6f2a4b091e5b61429e5a22b1b14c6477b3cab535
Axel <alundkvist97@gmail.com>
2022-01-12 11:14:27 +0100

    Adding restarts in the calendaring program

--- post_processing/Force_model_impact_on_electrode/particle_animation.py
+++ post_processing/Force_model_impact_on_electrode/particle_animation.py
@@ -4,7 +4,7 @@ from visualization_functions_3d.animation import Animation
 
 def main():
     print('start')
-    simulation_directory = 'c:/Users/Axel/Documents/DEM/results/electrode_calendaring/bt065N500'
+    simulation_directory = 'c:/Users/Axel/Documents/DEM/results/electrode_calendaring/bt065N3000'
     mlab.figure(size=(1920, 1200), bgcolor=(1., 1., 1.))
     animation = Animation(simulation_directory)
     animation.save_directory = simulation_directory+'/animation/imgs/'
@@ -12,7 +12,7 @@ def main():
     animation.delay = 1e-6
     animation.plot_periodic_bc = False
     animation.mirror_particles = False
-    animation.start_time = 3.4
+    animation.start_time = 0.6
     animation.run()
     mlab.show()
 
