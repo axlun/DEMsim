e3ee5fd2b641ca2d3d8853ca167bb21a53c740f1
Axel <alundkvist97@gmail.com>
2022-08-17 11:54:39 +0200

    Contact postprocess for bertil

--- post_processing/Bertil_functions/contact_processing.py
+++ post_processing/Bertil_functions/contact_processing.py
@@ -1,6 +1,6 @@
 import sys
 import os
-
+import re
 
 
 if __name__ == '__main__':
@@ -9,4 +9,18 @@ if __name__ == '__main__':
     print('Hello world')
 
     print(argument_string)
-    print(os.listdir(argument_string))
+    contact_files = os.listdir(argument_string)
+    print(contact_files)
+    time = []
+    contact_time_and_file_name_dict  = {}
+    for i in range(0,len(contact_files)):
+        contact_file = contact_files[i]
+        time_stamp = re.split(r'\Acontacts_',contact_file)
+        time_stamp = re.split(r'.dou\n\Z', time_stamp[1])
+        contact_file = re.split(r'\n\Z',contact_file)
+        time.append(float(time_stamp[0]))
+        contact_time_and_file_name_dict[time_stamp[0]] = contact_file[0]
+
+    print(contact_time_and_file_name_dict)
+    time.sort()
+    print(time)
