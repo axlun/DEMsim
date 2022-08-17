df0f8e33d01584b20529166020da3a4c5b46332a
Axel <alundkvist97@gmail.com>
2022-08-17 15:37:54 +0200

    Contact postprocess for bertil

--- post_processing/Bertil_functions/contact_processing.py
+++ post_processing/Bertil_functions/contact_processing.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
         contact_file = contact_files[i]
         time_stamp = re.split(r'\Acontacts_',contact_file)
         print(time_stamp[1])
-        time_stamp = re.split(r'\.dou\n\Z', time_stamp[1])
+        time_stamp = re.split(r'.dou\n\Z', time_stamp[1])
         print(time_stamp)
         contact_file = re.split(r'\n\Z',contact_file)
         print(contact_file)
