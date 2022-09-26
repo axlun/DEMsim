cd0435befc019ad79f6c1e1c3a8bfa8abbaa5a5c
Axel <alundkvist97@gmail.com>
2022-09-26 13:18:11 +0200

    update of restart_electrode_calendering_hertz.cpp simulation to make it dynamic (printing of outputs etc.)

--- src/simulations/Force_model_impact_on_electrode/restart_electrode_calendering_hertz.cpp
+++ src/simulations/Force_model_impact_on_electrode/restart_electrode_calendering_hertz.cpp
@@ -21,32 +21,31 @@ void DEM::restart_electrode_calendering_hertz(const std::string &settings_file_n
     using namespace std::chrono_literals;
 
     SimulationParameters parameters(settings_file_name);
+
     auto restart_file_name =parameters.get_parameter<std::string>("restart_file_name");
     auto output_directory = parameters.get_parameter<std::string>("output_dir");
-    double surface_velocity =parameters.get_parameter<double>("calendaring_surface_velocity"); //How was this chosen?
     auto calendering_height = parameters.get_parameter<double>("calendering_height");
     auto simulator = EngineType(restart_file_name);
     auto mat = dynamic_cast<const ElectrodeMaterial *>(simulator.get_material(0));
+
+//====================TIME STEP AND MASS SCALING========================================================================
+    double time_step = parameters.get_parameter<double>("time_step")*1e-6;
+    std::chrono::duration<double> time_step_us {time_step};
+    std::cout << "Time step is:" << time_step_us.count()*1E6 << "µs\n";
+    simulator.set_time_increment(time_step_us);
+    float mass_scaling_factor = parameters.get_parameter<float>("mass_scaling");
+    std::cout << "Mass scaling is:" << mass_scaling_factor << "\n";
+    simulator.set_mass_scale_factor(mass_scaling_factor); //Orig 1E2
+//======================================================================================================================
+
+//=======================REMOVE OLD OUTPUT==============================================================================
     auto Calendering_output = simulator.get_output("output_0");
     simulator.remove_output(Calendering_output);
-    auto restart_output = simulator.create_output(output_directory, 1E-2s);
-    restart_output->print_particles = true;
-    restart_output->print_surface_positions = true;
-    restart_output->print_kinetic_energy = true;
-    restart_output->print_contacts = true;
-    restart_output->print_surface_forces = true;
-    restart_output->print_fabric_force_tensor =true;
-    restart_output->print_periodic_bc = true;
-    restart_output->print_mirror_particles= true;
+//======================================================================================================================
 
     auto top_surface = simulator.get_surface<EngineType::PointSurfacePointer>("top_plate");
     auto deformable_surface = simulator.get_surface<EngineType::DeformablePointSurfacePointer>("bottom_plate");
-    std::cout << "****************Time to start the simulation**************** \n";
-
-
     std::cout << "Binder thickness fraction: " << mat->binder_thickness_fraction << "\n";
-
-
     auto max_binder_thickness = 0.;
     double particle_volume = 0.;
     for (auto& p: simulator.get_particles())
@@ -63,40 +62,74 @@ void DEM::restart_electrode_calendering_hertz(const std::string &settings_file_n
     std::cout << "Particle volume: " << particle_volume << "\n";
     std::cout << "Box side: " << box_side << "\n";
 
-
-
     //Calendaring process
-    EngineType::RunForTime run_for_time(simulator, .1E-0s);
-    std::cout << "****************Initialize calendaring process ****************\n";
     auto bbox = simulator.get_bounding_box(); //get the XYZ max/min that contain all particles
     double h_3 = bbox[5]; //height of uppermost particle (Z-max)
     std::cout<<"Heigh of uppermoast particle: "<< h_3<< std::endl;
+    std::cout<<"Calendering height : "<< calendering_height << std::endl;
     if (h_3<1.2*calendering_height){
+        std::cout<<"Heigh of uppermoast particle lower then 1.2 h_al: "<< h_3 << std::endl;
         h_3=1.2*calendering_height;
-        std::cout<<"Heigh of uppermoast particle lower then 1.2 h_al: "<< h_3<< std::endl;
-    }
-   //=================change this!!===================================
-    top_surface->move(-Vec3(0, 0,  initial_calender_surface_height - h_3-1.01*max_binder_thickness), Vec3(0, 0, 0)); //Move top surface to uppermost particle+binder thickness
-    //===================================================================
-    top_surface->set_velocity(Vec3(0,0,0.-surface_velocity/2));
-    std::chrono::duration<double> compaction_time_2 {((h_3+1.01*max_binder_thickness-calendering_height) / (surface_velocity/2))};
+        std::cout<<"Calednering surface changed to: "<< h_3 << std::endl;
 
-    std::cout<<"Compaction time: "<< compaction_time_2.count()<< std::endl;
-    run_for_time.reset(compaction_time_2);
+    }
+    EngineType::RunForTime run_for_time(simulator, .1E-0s);
+    std::cout << "****************Initialize calendaring process ****************\n";
+    top_surface->move(-Vec3(0, 0,  initial_calender_surface_height - h_3-1.01*max_binder_thickness), Vec3(0, 0, 0)); //Move top surface to uppermost particle+binder thickness
+
+//================================SET COMPACTION TIME AND FIND SURF. VELOCITY===========================================
+    double compaction_time_input = parameters.get_parameter<double>("compaction_time");
+    std::chrono::duration<double> fix_compaction_time (compaction_time_input);
+    auto calendering_distance = (h_3+1.01*max_binder_thickness-calendering_height);
+    auto fix_time_surface_vel = calendering_distance/fix_compaction_time.count();
+    std::cout << "Calendering surface velocity: " << fix_time_surface_vel << "m/s \n";
+
+//============================Calculate output frequency================================================================
+    double output_number =parameters.get_parameter<double>("output_number");
+    std::chrono::duration<double> output_interval {fix_compaction_time.count()/output_number};
+    std::cout << "Number of outputs: " << output_number << "\n";
+    std::cout << "Output interval: " << output_interval.count() << "s\n";
+
+    auto restart_output = simulator.create_output(output_directory, output_interval);
+    restart_output->print_particles = true;
+    restart_output->print_surface_positions = true;
+    restart_output->print_kinetic_energy = true;
+    restart_output->print_contacts = true;
+    restart_output->print_surface_forces = true;
+    restart_output->print_fabric_force_tensor =true;
+    restart_output->print_periodic_bc = true;
+    restart_output->print_mirror_particles= true;
+//======================================================================================================================
+
+    run_for_time.reset(fix_compaction_time);
+    top_surface->set_velocity(Vec3(0,0,-fix_time_surface_vel));
+    std::cout << "Calendering time: " << fix_compaction_time.count() << "s\n";
     simulator.run(run_for_time);
-    std::cout<<"Writing restart file\n";
-    simulator.write_restart_file(output_directory + "/compact_restart_file.res");
-    std::cout << "****************Initialize unloading ****************\n";
-    top_surface->set_velocity(Vec3(0, 0, surface_velocity/2));
-//    mat->adhesive = true; //Enable adhesive when particles are compacted and before unloaded
+//new comment
+
+//======================================================================================================================
+
+//======================================================================================================================
+
+//======================================================================================================================
+
+
+
+//=======================UNLOADING CALENDERING SURFACE TO ZERO FORCE=================================================
+    std::cout << "****************Unloading untill zero force on calendering surface ****************\n";
     EngineType::SurfaceNormalForceLess zero_force(top_surface, 0.);
-    // simulator.set_rotation(false);
+    top_surface->set_velocity(Vec3(0,0,fix_time_surface_vel));
     simulator.run(zero_force);
     top_surface->set_velocity(Vec3(0, 0, 0));
+//======================================================================================================================
+
+
     auto top_surface_position_after_calendering = top_surface->get_points()[0].z();
     std::cout<<"Top surface position after calendering: "<< top_surface_position_after_calendering<< std::endl;
-    std::cout << "****************Let rest for 1s ****************\n";
-    run_for_time.reset(1s);
+    std::cout << "****************Let rest for half of calendering time ****************\n";
+    std::cout << "Running for: " << (fix_compaction_time*0.5).count() << "s \n";
+
+    run_for_time.reset(fix_compaction_time*0.5);
     simulator.run(run_for_time);
     bbox = simulator.get_bounding_box(); //get the XYZ max/min that contain all particles
     double Active_layer_height = bbox[5];
@@ -115,4 +148,5 @@ void DEM::restart_electrode_calendering_hertz(const std::string &settings_file_n
     results_file << "Height of active layer=" << Active_layer_height << "\n";
     results_file << "Top surface position=" << top_surface_position_after_calendering << "\n";
     results_file.close();
+
     }
