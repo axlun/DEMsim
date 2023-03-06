dd5fc63d495de3775775a4610b86c11b90235976
Axel <alundkvist97@gmail.com>
2023-03-06 11:07:53 +0100

    Adding force_arrow_plotter and velocity_arrow_plotter

--- post_processing/visualization_functions_3d/animation.py
+++ post_processing/visualization_functions_3d/animation.py
@@ -3,18 +3,19 @@ import glob
 import os
 import re
 import time
+import pathlib
 
 import numpy as np
 from mayavi import mlab
 
-from visualization_functions_3d.plotting_functions import SpheresPlotter, BoundingBox, SurfacesPlotter, ForceArrowPlotter
+from visualization_functions_3d.plotting_functions import SpheresPlotter, BoundingBox, SurfacesPlotter, ForceArrowPlotter, ForceArrowPlotter2, VelocityArrowPlotter
 from visualization_functions_3d.periodic_bc import PeriodicBC
 from visualization_functions_3d import colors
 
 
 class Animation:
     def __init__(self, directory):
-        self.directory = directory
+        self.directory = pathlib.Path(directory)
         self.delay = 0.0
         self.nth_timeframe = 1
         self.start_time = 0
@@ -35,8 +36,12 @@ class Animation:
         self.zoom_settings = None
         self.plot_periodic_bc = False
         self.periodic_bc_plotter = None
+        self.force_arrow_plotter = None
+        self.velocity_arrow_plotter = None
         self.mirror_particles = False
         self.plot_force_arrow = False
+        self.plot_velocity_arrow = False
+        self.n_vel = -1
         self.n_force = -1
         self.spheres_plotter = SpheresPlotter()
         self.mirror_particles_plotter = SpheresPlotter(color=colors.silver)
@@ -52,7 +57,7 @@ class Animation:
 
     def initialize(self):
         if self.view_surfaces:
-            self.surfaces_plotter = SurfacesPlotter(self.directory + '/surface_positions.dou', self.surfaces_colors,
+            self.surfaces_plotter = SurfacesPlotter(self.directory / 'surface_positions.dou', self.surfaces_colors,
                                                     self.surfaces_opacities, self.plot_order, self.bounding_boxes,
                                                     self.visible_functions)
             self.surfaces_plotter.surfaces_opacities = self.surfaces_opacities
@@ -62,13 +67,16 @@ class Animation:
             self.surfaces_plotter.visible_times = self.visible_functions
 
         if self.plot_periodic_bc:
-            self.periodic_bc_plotter = PeriodicBC(self.directory + '/periodic_bc.dou')
+            self.periodic_bc_plotter = PeriodicBC(self.directory / 'periodic_bc.dou')
 
         if self.plot_force_arrow:
-            self.force_arrow_plotter = ForceArrowPlotter(colors.green, 1. ,self.n_force)
+            # self.force_arrow_plotter = ForceArrowPlotter(colors.green, 1., n_force=self.n_force)
+            self.force_arrow_plotter = ForceArrowPlotter2(colors.green, 1.,)
 
+        if self.plot_velocity_arrow:
+            self.velocity_arrow_plotter = VelocityArrowPlotter(colors.red, 1., n_velocity=self.n_vel)
 
-        particle_files = glob.glob(self.directory + '/particles/particles_*.dou')
+        particle_files = glob.glob(str(self.directory) + '/particles/particles_*.dou')
         particle_files = [os.path.basename(particle_file) for particle_file in particle_files]
         print(self.nth_timeframe)
         particle_files = particle_files[::self.nth_timeframe]
@@ -107,26 +115,30 @@ class Animation:
             if string[::-1].find('.') < self.max_decimal_points:
                 string += "0"*(self.max_decimal_points-int(string[::-1].find('.')))
             file_name_str.append(string)
+
+        surface_position_data =np.genfromtxt(self.directory / 'surface_positions.dou', delimiter=',')
         for i, t in enumerate(self.frame_times):
-            print(i)
-            print(self.frame_times[i])
-            particle_data = np.genfromtxt(self.directory + '/particles/particles_' + t + '.dou', delimiter=',')
+            print('Frame number: '+ str(i))
+            print('Frame time: ' + str(t))
+            particle_data = np.genfromtxt(self.directory / str('particles/particles_' + str(t) + '.dou'), delimiter=',')
             self.spheres_plotter.plot(particle_data)
             if self.mirror_particles:
-                mirror_particle_data = np.genfromtxt(self.directory + '/mirror_particles/mirror_particles_'
-                                                     + t + '.dou', delimiter=',')
+                mirror_particle_data = np.genfromtxt(self.directory / str('mirror_particles/mirror_particles_'
+                                                     + t + '.dou'), delimiter=',')
                 self.mirror_particles_plotter.plot(mirror_particle_data)
             if self.surfaces_plotter:
                 self.surfaces_plotter.plot(float(t))
             if self.plot_periodic_bc:
                 self.periodic_bc_plotter.plot(float(t))
-
             if self.plot_force_arrow:
                 self.force_arrow_plotter.plot(particle_data)
+            if self.plot_velocity_arrow:
+                self.velocity_arrow_plotter.plot(self.directory, float(t), particle_data, surface_position_data)
 
             f = mlab.gcf()
             f.scene.render()
-
+            # mlab.colorbar()
+            # mlab.axes()
             if self.save_frames:
                 if self.figure_directory is None:
                     self.figure_directory = self.directory
@@ -142,4 +154,10 @@ class Animation:
                 mlab.savefig(filename=self.save_directory + name)
 
             time.sleep(self.delay)
+            if self.plot_velocity_arrow:
+                self.velocity_arrow_plotter.clear_arrows()
+            # if self.plot_force_arrow:
+            #     self.force_arrow_plotter.clear_arrows()
+
+
             yield
