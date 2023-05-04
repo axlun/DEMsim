768077bce8e5be0b3f3b224682ad015964d50f0b
Axel <alundkvist97@gmail.com>
2023-05-04 13:36:33 +0200

    Added new calculation of step time

--- post_processing/force_model_impact_on_calendering/Rayleigh_time_step.py
+++ post_processing/force_model_impact_on_calendering/Rayleigh_time_step.py
@@ -3,7 +3,7 @@ import numpy as np
 if __name__ == '__main__':
 
     r_min = 4e-2 # [m]
-    mass_scaling = 1e2 # [-]
+    mass_scaling = 1e0 # [-]
     rho = 4630 # [kg / m3]
     m_min = rho * 4 * 3.1415 / 3 * r_min**3.0 # [kg]
     R_0 = r_min / 2 # [m]
@@ -13,11 +13,23 @@ if __name__ == '__main__':
     k_p = 4 / 3 * E_eff * R_0**.5 # []
     yield_disp_coeff = 8.59e-3
     h_yield = R_0 * yield_disp_coeff #
-    k = 3 / 2 * k_p * h_yield**.5
-    delta_t_min = 2*(mass_scaling * m_min / k)**.5
+    k_hertz = 3 / 2 * k_p * h_yield**.5
+    delta_t_min_hertz = 2*(mass_scaling * m_min / k_hertz)**.5
 
-#    print("{0:.2E}".format(delta_t_min*1e6))
+    print("The maximum time step size is " +"{0:.2E}".format(delta_t_min_hertz*1e6)+'µs for the Hertz contact')
 
-    print("The maximum time step size is " +"{0:.2E}".format(delta_t_min*1e6)+'µs')
+    print("Maximum time step with safety factor of 20 is: "+ "{0:.2E}".format(delta_t_min_hertz*1e6/20)+'µs for the Hertz contact' )
 
-    print("Maximum time step with safety factor of 20 is: "+ "{0:.2E}".format(delta_t_min*1e6/20)+'µs' )
+
+    # Rigid plastic contact
+    H_bar_max = 2.8
+    C_2_max = 1.43
+    sigma_Y = 4000e6
+    k_rigid_plastic = 3.14*C_2_max*H_bar_max*2*R_0*sigma_Y
+    delta_t_min_rigid_plastic = 2 * (mass_scaling * m_min / k_rigid_plastic) ** .5
+    print("The maximum time step size is " +"{0:.2E}".format(delta_t_min_rigid_plastic*1e6)+'µs for the rigid plastic '
+                                                                                            'contact')
+
+    print("Maximum time step with safety factor of 20 is: "+ "{0:.2E}".format(delta_t_min_rigid_plastic*1e6/20)
+          +'µs for the rigid plastic contact' )
+
