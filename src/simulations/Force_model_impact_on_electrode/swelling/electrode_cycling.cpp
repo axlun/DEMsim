bd5f5da0be4c45e0b18f485fc19bd2e834304c5f
Axel <alundkvist97@gmail.com>
2024-08-28 11:19:33 +0200

    Updated cycling simulation.

--- src/simulations/Force_model_impact_on_electrode/swelling/electrode_cycling.cpp
+++ src/simulations/Force_model_impact_on_electrode/swelling/electrode_cycling.cpp
@@ -30,6 +30,12 @@ void DEM::electrode_cycling(const std::string &settings_file_name)
 
     std::chrono::duration<double> cycling_time{parameters.get_parameter<double>("cycling_time")};
 
+    auto restart_array = parameters.get_vector<unsigned>("restart_array");
+
+//    auto compression_pressure = parameters.get_parameter<double>("compression_pressure");
+//    auto surface_adhesion = parameters.get_parameter<bool>("surface_adhesion");
+//    auto surface_velocity = parameters.get_parameter<double>("surface_velocity");
+
 //==================SET TIME STEP AND MASS SCALING=======================================================================
     double time_step = parameters.get_parameter<double>("time_step") * 1e-6;
     std::chrono::duration<double> time_step_us{time_step};
@@ -58,51 +64,55 @@ void DEM::electrode_cycling(const std::string &settings_file_name)
 
 //======================Set swelling/scaling rate and time======================================================================
 
-    auto swell_state = parameters.get_parameter<double>("swell_state");
-    double swell_rate = (swell_state - 1) / cycling_time.count();
-    std::cout << "swell_rate = " << swell_rate << std::endl;
+    auto swell_states = parameters.get_vector<double>("swell_states");
+    auto material_scalings = parameters.get_vector<double>("material_scalings");
 
-    auto material_scaling = parameters.get_parameter<double>("material_scaling");
-    double material_scaling_rate = (material_scaling - 1) / cycling_time.count();
+    if ( swell_states.size() != material_scalings.size() ) throw std::invalid_argument("swell_states and material_scaling arrays must be of same size!\n");
+    std::cout << "Number of cycles for simulation: " <<  swell_states.size()/2 << "\n";
 
-    auto states_of_charge = parameters.get_vector<double>("states_of_charge");
-
-    // Set up so swelling/scaling rate is constant, and the time betweeel 0 - 100% SOC is prescribed.
-    std::vector < std::pair < int, std::chrono::duration < double>>> charging_directions_and_times;
-    double SOC = 0.0;
-    int charge_direction = 0;
-    for (int i = 0; i < states_of_charge.size(); ++i)
+    // setup so swell_rate is constant (determined from first charge), charging time is determined from swell state and
+    // material_scaling_rate is determined from scaling_factor and swellling_time.
+    std::vector < std::tuple < int, double, std::chrono::duration < double>>> charging_direction_scale_rate_and_times;
+    std::vector < std::tuple < double, double>> swell_and_scale_rates;
+    double swell_state = 1.0;
+    double material_scaling_state = 1.0;
+//    int swell_direction = 0;
+    for (int i = 0; i < swell_states.size(); ++i)
     {
-        double to_be_SOC = states_of_charge[i];
-        double SOC_diff = to_be_SOC - SOC;
-        charge_direction = -1;
-        if (SOC_diff >= 0) charge_direction = 1;
-        std::chrono::duration<double> charge_time{cycling_time * abs(SOC_diff) / 100.0};
-        charging_directions_and_times.push_back(std::make_pair(charge_direction, charge_time));
-        SOC = to_be_SOC;
+        double to_be_swell_state = swell_states[i];
+        double swell_state_diff = to_be_swell_state - swell_state;
+        double swell_rate = swell_state_diff/ cycling_time.count();
+
+        double to_be_material_scaling = material_scalings[i];
+        double material_scaling_diff = to_be_material_scaling - material_scaling_state;
+        double material_scaling_rate = material_scaling_diff/cycling_time.count();
+
+        swell_and_scale_rates.push_back(std::make_tuple(swell_rate, material_scaling_rate));
+        std::cout << "Charge step " << i << ", swell_rate = " << swell_rate <<
+                  ", material_scaling_rate = " << material_scaling_rate << "\n";
+        swell_state = to_be_swell_state;
+        material_scaling_state = to_be_material_scaling;
     }
 
     std::chrono::duration<double> resting_time{cycling_time.count() / 10.};
     std::cout << "Running for resting time: " << resting_time.count() << std::endl;
     EngineType::RunForTime run_for_resting_time(simulator, resting_time);
-    simulator.run(run_for_resting_time);
+//    simulator.run(run_for_resting_time);
 
     //==PLACE TOP SURFACE ON ACTIVE LAYER===============================================================================
     //move top_surface "closer" to active layer
+    /*
     auto top_surface = simulator.get_surface<EngineType::PointSurfacePointer>("top_plate");
 
     auto surface_area = pow(2*top_surface->get_points()[0][0], 2);
     std::cout << "surface_area = " << surface_area << std::endl;
-    auto compression_pressure = parameters.get_parameter<double>("compression_pressure");
 
     auto compression_force = compression_pressure * 1E6 * surface_area;
     std::cout << "compression_force = " << compression_force << std::endl;
 
     auto bottom_surface = simulator.get_surface<EngineType::DeformablePointSurfacePointer>("bottom_plate");
 
-    auto surface_velocity = parameters.get_parameter<double>("surface_velocity");
     top_surface->set_velocity(Vec3(0,0,-surface_velocity));
-    auto surface_adhesion = parameters.get_parameter<bool>("surface_adhesion");
     top_surface->set_adhesive(surface_adhesion);
     std::cout << "**************** Moving top surface with velocity "<< surface_velocity << ", to pressure " <<
         compression_pressure <<" ****************\n";
@@ -111,52 +121,49 @@ void DEM::electrode_cycling(const std::string &settings_file_name)
     simulator.run(compaction_force);
 
     top_surface->set_velocity(Vec3(0, 0, 0));
+    */
+    //==================================================================================================================
+    //=MOVE TOP SURFACE FROM LAYER======================================================================================
+    auto calendering_surface = simulator.get_surface<EngineType::PointSurfacePointer>("top_plate");
+    //Remove calendering surface from RVE
+    auto bbox = simulator.get_bounding_box(); //get the XYZ max/min that contain all particles
+    double h_1 = bbox[5]; //height of uppermost particle (Z-max)
+    double surface_removal_distance = h_1 + 1 - calendering_surface->get_points()[0].z();
+    calendering_surface->move(Vec3(0, 0,  surface_removal_distance), Vec3(0, 0, 0));
     //==================================================================================================================
 
+    // Give array of after which cycles a restart should be written.
     unsigned counter{0};
-    for (const auto &charge_iterator: charging_directions_and_times)
+    unsigned cycle{0};
+    for (const auto &charge_iterator: swell_and_scale_rates)
     {
-        std::cout << "Charge step " << counter << ", charging direction = " << charge_iterator.first <<
-                  ", charging time = " << charge_iterator.second.count() << std::endl;
+        std::cout << "Charge step " << counter << ", swell_rate = " << std::get<0>(charge_iterator) <<
+                  ", material_scaling_rate = " << std::get<1>(charge_iterator) << "\n"<< std::endl;
         for (auto &p: simulator.get_particles())
         {
-            p->set_swell_rate(charge_iterator.first * swell_rate);
-            p->set_material_scale_rate(charge_iterator.first * material_scaling_rate);
+            p->set_swell_rate(std::get<0>(charge_iterator));
+            p->set_material_scale_rate(std::get<1>(charge_iterator));
         }
 
-        EngineType::RunForTime charging_run(simulator, charge_iterator.second);
+        EngineType::RunForTime charging_run(simulator, cycling_time);
         simulator.run(charging_run);
         counter++;
+        cycle = counter / 2;
+        auto cycle_itr = std::find(restart_array.begin(), restart_array.end(), cycle);
+        if ( cycle_itr != restart_array.end())
+        {
+            for (auto &p: simulator.get_particles())
+            {
+                p->set_swell_rate(0);
+                p->set_material_scale_rate(0);
+            }
 
+            std::cout << "Writing restart file after cycle: " << cycle << "\n";
+            restart_array.erase(cycle_itr);
+            std::stringstream restart_file_name;
+            restart_file_name << "/electrode_cycling_cycle_" << cycle << ".res";
+            simulator.write_restart_file(output_directory + restart_file_name.str());
+        }
     }
     std::cout << "**************** Simulation finalized ****************\n";
 }
-/*
-
-    for (int i = 0; i < swell_times.size(); i++)
-    {
-        std::cout << "Swelling step " << i << ", swelling for time: " << swell_times[i].count() << std::endl;
-        for (auto &p: simulator.get_particles())
-        {
-            p->set_swell_rate(swell_rate);
-            p->set_material_scale_rate(material_scaling_rate);
-        }
-        EngineType::RunForTime run_for_swelling_time(simulator, swell_times[i]);
-        simulator.run(run_for_swelling_time);
-        std::cout << "Swelling finished, resting for " << resting_time.count() << "s" << std::endl;
-        for (auto &p: simulator.get_particles())
-        {
-            p->set_swell_rate(0.);
-            p->set_material_scale_rate(0.);
-        }
-        run_for_resting_time.reset(resting_time);
-        simulator.run(run_for_resting_time);
-        std::cout << "**************** Simulation finished ****************" << std::endl;
-        std::cout << "Writing restart file " << std::endl;
-        std::stringstream restart_file_name;
-        restart_file_name << "/electrode_swell_state_" << swell_states[i] << ".res";
-        simulator.write_restart_file(output_directory + restart_file_name.str());
-    }
-    std::cout << "**************** Simulation finalized ****************\n";
-}
- */
