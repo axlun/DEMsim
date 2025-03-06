e41588fd26dd1799c3c148bb52bb8f168bfa8f80
Axel <alundkvist97@gmail.com>
2025-03-06 10:59:07 +0100

    Handeling empty contact files in binder_fracture_processing.py

--- post_processing/Bertil_functions/binder_fracture_processing.py
+++ post_processing/Bertil_functions/binder_fracture_processing.py
@@ -23,19 +23,21 @@ if __name__ == '__main__':
         key = str(time[i])
         if time[i].is_integer():
             key = str(int(time[i]))
-        file_to_open = argument_string+'/'+contact_time_and_file_name_dict[key]
-        data = pd.read_csv(file_to_open,header=None).to_numpy()
         active_binder_contact = 0
         binder_contact = 0
         fractured_binder_contact = 0
-        for j in range(0, len(data[:, 0])):
-            if data[j, 6] != 0 and (data[j, 6]) == (data[j, 7]):
-                active_binder_contact += 1
-            if data[j, 21] == 1 :
-                fractured_binder_contact += 1
-            if data[j, 19] == 1 :
-                binder_contact += 1
-
+        file_to_open = argument_string + '/' + contact_time_and_file_name_dict[key]
+        try:
+            data = pd.read_csv(file_to_open,header=None).to_numpy()
+            for j in range(0, len(data[:, 0])):
+                if data[j, 6] != 0 and (data[j, 6]) == (data[j, 7]):
+                    active_binder_contact += 1
+                if data[j, 21] == 1 :
+                    fractured_binder_contact += 1
+                if data[j, 19] == 1 :
+                    binder_contact += 1
+        except:
+           data = data
         binder_contact_vec.append(binder_contact)
         active_binder_contact_vec.append(active_binder_contact)
         fractured_binder_contact_vec.append(fractured_binder_contact)
